3512596f712d9963418deded0807236a50f64f54
paula <paula.millanii@gmail.com>
2021-09-01 20:14:27 -0300

    two players

--- app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pedrapapeltesoura/MainActivity.java
@@ -4,36 +4,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+
+import java.util.Random;
+
+import br.edu.ifsp.scl.sdm.pedrapapeltesoura.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private RadioGroup opcaoRg;
-    private Button pedraBt, papelBt, tesouraBt;
-    private TextView resultadoTv;
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        opcaoRg = findViewById(R.id.opcaoRg);
-        pedraBt = findViewById(R.id.pedraBt);
-        papelBt = findViewById(R.id.papelBt);
-        tesouraBt = findViewById(R.id.tesouraBt);
+        setContentView(activityMainBinding.getRoot());
 
-        opcaoRg.setOnClickListener(this);
-        pedraBt.setOnClickListener(this);
-        papelBt.setOnClickListener(this);
-        tesouraBt.setOnClickListener(this);
+        activityMainBinding.opcaoRg.setOnClickListener(this);
+        activityMainBinding.zeroBt.setOnClickListener(this);
+        activityMainBinding.umBt.setOnClickListener(this);
+        activityMainBinding.doisBt.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+        int jogadaHumano = -1;
+
+        switch (view.getId()) {
+            case R.id.zeroBt:
+                jogadaHumano = 0;
+                break;
+            case R.id.umBt:
+                jogadaHumano = 1;
+                break;
+            case R.id.doisBt:
+                jogadaHumano = 2;
+                break;
+        }
+
+        jogar(jogadaHumano);
+    }
+
+    // 0 -> pedra
+    // 1 -> papel
+    // 2 -> tesoura
+
+    private void jogar(int jogadaHumano) {
+        Random random = new Random((System.currentTimeMillis()));
+        int jogadaComputador1 = random.nextInt(3);
+        int jogadaComputador2 = random.nextInt(3);
+
+        StringBuilder resultadoSb = new StringBuilder();
+        resultadoSb.append(" Sua jogada: ");
+        resultadoSb.append(jogadaHumano);
+        resultadoSb.append("\n");
+        resultadoSb.append(" Computador: ");
+        resultadoSb.append(jogadaComputador1);
+        resultadoSb.append("\n");
+        resultadoSb.append(" Computador: ");
+        resultadoSb.append(jogadaComputador2);
+        resultadoSb.append("\n");
+
+        if (activityMainBinding.opcaoRg.getCheckedRadioButtonId() == R.id.doisRb) {
+
+            //dois jogadores
+            if (jogadaHumano == jogadaComputador1) {
+                resultadoSb.append("Empate");
+            } else {
+                if ((jogadaHumano == 0 && jogadaComputador1 == 2) || (jogadaHumano == 1 && jogadaComputador1 == 0) || (jogadaHumano == 2 && jogadaComputador1 == 1)) {
+                    resultadoSb.append("Você Venceu!");
+                } else {
+                    resultadoSb.append("Computador Venceu!");
+                }
+            }
+
+        //tres jogadores
+        } else {
+            resultadoSb.append("  deus ajude");
+
+        }
+
+        activityMainBinding.resultadoTv.setText(resultadoSb.toString());
+
 
     }
 }
